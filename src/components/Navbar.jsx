8608b75b6f898506cf96024822f7e55b41231037
Damian <dd.laskowski@gmail.com>
2025-07-20 20:45:48 +0200

    Implement Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             {backgroundColor: 'transparent'},
             {
                 backgroundColor: '#00000050',
-                backgroundFilter: 'blur(10px)',
+                backgroundFilter: 'blur(1px)',
                 duration: 1,
                 ease: 'power1.inOut',
             },
